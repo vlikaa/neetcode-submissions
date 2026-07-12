9b5b9723f95b9cac28a422b3a707790b0b0426cd
vlikaa <149687088+vlikaa@users.noreply.github.com>
2026-07-12 09:31:25 +0400

    Add: top-k-elements-in-list - submission-6

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-6.cs
@@ -0,0 +1,42 @@
+public class Solution {
+    public int[] TopKFrequent(int[] nums, int k) 
+    {
+        var dict = new Dictionary<int, int>();
+        foreach (var n in nums)
+        {
+            dict[n] = dict.GetValueOrDefault(n, 0) + 1;
+        }
+
+        var freq = new List<int>[dict.Values.Max() + 1];
+        foreach (var kvp in dict)
+        {
+            if (freq[kvp.Value] == null)
+            {
+                freq[kvp.Value] = [];
+            }
+
+            freq[kvp.Value].Add(kvp.Key);
+        }
+
+        var result = new List<int>();
+        for (var i = freq.Length - 1; i > 0; --i)
+        {
+            if (freq[i] == null)
+            {
+                continue;
+            }
+
+            foreach (var n in freq[i])
+            {
+                result.Add(n);
+
+                if (result.Count == k)
+                {
+                    return result.ToArray();
+                }
+            }
+        }
+
+        return result.ToArray();
+    }
+}
