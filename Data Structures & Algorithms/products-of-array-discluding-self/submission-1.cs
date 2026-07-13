be2b7770bc77a44fd1506d943954e6e4ea49a46a
vlikaa <149687088+vlikaa@users.noreply.github.com>
2026-07-13 18:18:02 +0400

    Add: products-of-array-discluding-self - submission-1

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-1.cs
@@ -0,0 +1,21 @@
+public class Solution {
+    public int[] ProductExceptSelf(int[] nums) {
+        var result = new int[nums.Length];
+
+        var prefix = 1;
+        for (var i = 0; i < nums.Length; ++i)
+        {
+            result[i] = prefix;
+            prefix *= nums[i];
+        }
+
+        var postfix = 1;
+        for (var i = nums.Length - 1; i >= 0; --i)
+        {
+            result[i] *= postfix;
+            postfix *= nums[i];
+        }
+
+        return result;
+    }
+}
