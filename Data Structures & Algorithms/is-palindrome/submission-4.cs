7bfbe1f911b1b4c46f3f87dda00e2448e1607c7a
vlikaa <149687088+vlikaa@users.noreply.github.com>
2026-07-15 14:02:37 +0400

    Add: is-palindrome - submission-4

---
+++ Data Structures & Algorithms/is-palindrome/submission-4.cs
@@ -0,0 +1,21 @@
+public class Solution {
+    public bool IsPalindrome(string s) {
+        int i = 0, j = s.Length - 1;
+
+        while (i < j)
+        {
+            while (i < j && !char.IsAsciiLetterOrDigit(s[i])) ++i;
+            while (i < j && !char.IsAsciiLetterOrDigit(s[j])) --j;
+            
+            if (char.ToLower(s[i]) != char.ToLower(s[j]))
+            {
+                return false;
+            }
+
+            ++i;
+            --j;
+        }
+
+        return true;
+    }
+}
