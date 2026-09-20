8f2721ce7ccd3a20eb50926fd0960428064b5640
vlikaa <149687088+vlikaa@users.noreply.github.com>
2026-09-20 23:14:39 +0400

    Add: is-anagram - submission-11

---
+++ Data Structures & Algorithms/is-anagram/submission-11.cs
@@ -0,0 +1,33 @@
+public class Solution {
+    public bool IsAnagram(string s, string t) {
+        var dict = new Dictionary<char, int>();
+
+        foreach (var c in s)
+        {
+            if (!dict.TryAdd(c, 1))
+            {
+                ++dict[c];
+            }
+        }
+
+        foreach (var c in t)
+        {
+            if (!dict.TryAdd(c, -1))
+            {
+                --dict[c];
+            }
+        }
+
+        foreach (var v in dict.Values)
+        {
+            if (v != 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
+
+
